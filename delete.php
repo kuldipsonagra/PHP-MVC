b7a9d11885e875e67f9d0ec20f0feecbb93c06ee
kuldip~ <kuldips.codershut@gmail.com>
2024-04-23 22:40:37 -0700

    COMPLETPHPCRUD

---
+++ delete.php
@@ -0,0 +1,8 @@
+<?php
+include ('connection.php');
+$deleteId = $_GET['deleteId'];
+print_r($deleteId) ;
+$sql = "DELETE FROM `phpcrud` WHERE id=$deleteId";
+$result = mysqli_query($conn, $sql);
+header('location:mainForm.php');
+?>
---
+++ validation.php
@@ -0,0 +1,58 @@
+<?php
+session_start();
+
+include ('connection.php');
+
+$updateId = $_POST['updateId'];
+$name = $_POST['name'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$gender = isset($_POST['gender']) ? $_POST['gender'] : '';
+$image = $_FILES['image'];
+$errors = [];
+$values = [];
+
+if (empty($name)) {
+    $errors['name'] = 'First Name is required';
+}
+if (empty($email)) {
+    $errors['email'] = 'Email is required';
+}
+if (empty($password)) {
+    $errors['password'] = 'Password is required';
+}
+if (empty($gender)) {
+    $errors['gender'] = 'Gender is required';
+}
+
+if ($image) {
+    $filename = $_FILES['image']['name'];
+    $tmpname = $_FILES['image']['tmp_name'];
+    move_uploaded_file($tmpname, 'images/' . $filename);
+}
+
+if (!empty($errors)) {
+    $_SESSION['errors'] = $errors;
+    // $_SESSION['values'] = $values;
+    $_SESSION['values'] = $_POST;
+    header('Location:mainForm.php?updateid='.$updateId.'');
+} else {
+    if (isset($_POST['submit'])) {
+
+        $sql = "INSERT INTO `phpcrud`( `name`, `email`, `password`,`gender`,`image`) VALUES ('$name','$email','$password','$gender','$filename')";
+        $result = mysqli_query($conn, $sql);
+        session_destroy();
+        header('Location:mainForm.php');
+    }
+    if(isset($_POST['update'])){
+        $sql = "UPDATE `phpcrud` SET `name`='$name',`email`='$email',`password`='$password',`gender`='$gender',`image`='$filename' WHERE id=$updateId";
+        $result = mysqli_query($conn, $sql);
+
+        header('Location:mainForm.php');
+
+
+    }
+
+}
+
+?>
